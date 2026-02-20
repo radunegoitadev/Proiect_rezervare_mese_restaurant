9690801664c9515e2862666979e629e967878e7b
Radunegoita252005 <radu.negoita.dev@gmail.com>
2026-02-20 13:17:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from passlib.context import CryptContext
 from jose import jwt
+import uvicorn
 
 Secret_key = "12334555#343907545433##@@"
 
@@ -15,26 +16,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def getpswdhash(password):
     return pwd_context.hash(password)
-
-
-database_url = "sqlite:///./rezervari.db"
-engine = create_engine(database_url, connect_args={"check_same_thread": False})
-sesiune_locala = sessionmaker(autoflush = False, autocommit = False, bind=engine)
-baza = declarative_base()
-
-class rezervareDB(baza):
-    __tablename__ = "Rezervari"
-    id = Column(Integer, primary_key=True, index=True)
-    nume_client = Column(String)
-    masa = Column(Integer)
-    check_in = Column(DateTime)
-    check_out = Column(DateTime)
-
-class userDB(baza):
-    __tablename__ = "Users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    parola_criptata = Column(String)
 
 def verificare_parola(parola_normala:str,parola_criptata:str):
     return pwd_context.verify(parola_normala,parola_criptata)
@@ -44,6 +25,37 @@ def create_token(username: str):
     payload = {"exp": expiration, "sub": username}
     token = jwt.encode(payload, Secret_key, algorithm="HS256")
     return token
+
+database_url = "sqlite:///./rezervari.db"
+engine = create_engine(database_url, connect_args={"check_same_thread": False})
+sesiune_locala = sessionmaker(autoflush = False, autocommit = False, bind=engine)
+baza = declarative_base()
+
+class rezervare(BaseModel): 
+    nume : str
+    masa : int
+    start : datetime
+    end : datetime
+
+class UserCreate(BaseModel):
+    username : str
+    password : str
+
+class rezervareDB(baza):
+    __tablename__ = "Rezervari"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    nume_client = Column(String)
+    masa = Column(Integer)
+    check_in = Column(DateTime)
+    check_out = Column(DateTime)
+
+class userDB(baza):
+    __tablename__ = "Users"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    username = Column(String, unique=True, index=True)
+    parola_criptata = Column(String)
+
+baza_date: list[rezervare] = []
 
 baza.metadata.create_all(bind = engine)
 
@@ -56,36 +68,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class rezervare(BaseModel):
-    id : int 
-    masa : int
-    nume : str
-    start : datetime
-    end : datetime
-
-baza_date: list[rezervare] = []
-
-@app.post("/rezervari")
-def adauga_rezervare(informatii : rezervare):
+@app.post("/Adaugarerezervare")
+def adauga_rezervare(interval: rezervare):
     db = sesiune_locala()
-    for i in baza_date:
-        if(i.id == informatii.id):
-            raise HTTPException(status_code=400, detail=f"ID-ul cu numarul {i.id} există deja la clientul {i.nume}!")
-        if(i.masa == informatii.masa):
-            if(informatii.start < i.end and informatii.end > i.start):
-                raise HTTPException(status_code=400, detail="Masa este ocupată!")
-    noua_rezervare = rezervareDB(
-        id = informatii.id,
-        nume_client = informatii.nume,
-        masa = informatii.masa,
-        check_in = informatii.start,
-        check_out = informatii.end
-    )
-    db.add(noua_rezervare)
-    db.commit()
-    db.refresh(noua_rezervare)
-    db.close()
-    return {"Status": "Salvat cu succes in baza de date"}    
+    conflict = db.query(rezervareDB).filter(
+        rezervareDB.masa == interval.masa,
+        interval.start < rezervareDB.check_out,
+        interval.end > rezervareDB.check_in
+    ).first()
+    if conflict:
+        db.close()
+        raise HTTPException(status_code=400,detail="Intervalul selectat nu este disponibil")
+    else:
+        noua_rezervare = rezervareDB(nume_client = interval.nume, masa = interval.masa, check_in = interval.start, check_out = interval.end)
+        db.add(noua_rezervare)
+        db.commit()
+        db.close()
+        return{"Status": "Rezervarea a fost salvata in baza de date"}
+
 
 @app.get("/toaterezervarile")
 def primesterezervari():
@@ -123,22 +123,45 @@ def Afiseaza_rezervarea_id(id: int):
     raise HTTPException(status_code=404, detail=f"Rezervarea cu numarul {id} nu a fost gasita")
 
 @app.post("/register")
-def inregistrarea_utilizatorului(nume:str, password:str):
+def inregistrarea_utilizatorului(user_Data : UserCreate):
+    if(user_Data.password.__len__() < 8):
+        return {"Error": "Parola trebuie sa contina minim 8 caractere"}
+    elif not any(char.isupper() for char in user_Data.password):
+        return {"Error": "Parola trebuie sa contina minim o litera mare"}
+    elif not any(not char.isalnum() for char in user_Data.password):
+        return {"Error": "Parola trebuie sa contina minim un simbol"}
+    elif not any(char.isdigit() for char in user_Data.password):
+        return {"Error": "Parola trebuie sa contina cel putin un numar"}
     db = sesiune_locala()
-    parola = getpswdhash(password)
-    nou_user = userDB(username = nume, parola_criptata = parola)
-    db.add(nou_user)
-    db.commit()
-    db.close()
-    return {"Status": "Utilizator înregistrat cu succes"}
+    try:
+        parola = getpswdhash(user_Data.password)
+        nou_user = userDB(username = user_Data.username, parola_criptata = parola)
+        db.add(nou_user)
+        db.commit()
+        return{"Status": "Utilizator inregistrat cu succes"}
+    except Exception as e:
+        db.rollback()
+    finally:
+        db.close()
 
-@app.post("/login")
-def login(username:str, parola:str):
+@app.post("/Login")
+def login(user_data : UserCreate):
     db = sesiune_locala()
-    user = db.query(userDB).filter(userDB.username == username).first()
-    if(not user or not verificare_parola(parola, user.parola_criptata)):
-        raise HTTPException(status_code=404, detail="Username-ul sau parola invalide")
+    user = db.query(userDB).filter(userDB.username == user_data.username).first()
+    if(not user or not verificare_parola(user_data.password, user.parola_criptata)):
+        db.close()
+        raise HTTPException(status_code=404, detail="Username-ul sau parola invalide") 
     else:
-        token = create_token(username)
+        token = create_token(user_data.username)
+        db.close()
         return{"access_token": token, "token_type": "bearer"}
 
+@app.get("/Utilizatori")
+def vizualizare_utilizatori():
+    db = sesiune_locala()
+    Utilizatori_inregistrati_inbaza = db.query(userDB).all()
+    db.close()
+    return Utilizatori_inregistrati_inbaza
+    
+if __name__=="__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
