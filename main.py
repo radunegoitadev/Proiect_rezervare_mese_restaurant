091bc04bba315c270c32fea186d30482b9451400
Radunegoita252005 <radu.negoita.dev@gmail.com>
2026-02-20 16:51:20 +0200

    Am adugat dockerfile

--- main.py
+++ main.py
@@ -164,4 +164,4 @@ def vizualizare_utilizatori():
     return Utilizatori_inregistrati_inbaza
     
 if __name__=="__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
